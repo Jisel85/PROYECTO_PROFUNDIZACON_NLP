908439855c499ed91d2a2c4a80789a9522410c78
lacamposm <lacamposm@unal.edu.co>
2023-09-04 16:54:37 -0500

    * Created write_info_sentence_in_collection method in  Sentences' class

--- utilis/sentences.py
+++ utilis/sentences.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 import re
 import json
 import logging
@@ -12,6 +13,7 @@ logging.basicConfig(
 )
 
 MAIN_PATH = Path(os.getcwd())
+FOLDER_TO_READ = MAIN_PATH / "data" / "sentences_webscraping"
 url_base = "https://www.corteconstitucional.gov.co/relatoria/"
 
 
@@ -21,7 +23,7 @@ def get_url_sentence(sentence):
     AUTOS:              https://www.corteconstitucional.gov.co/relatoria/autos/1992/A024-92.htm
     CONSTITUCIONAL:     https://www.corteconstitucional.gov.co/relatoria/1992/C-587-92.htm
     :param sentence:
-    :return:
+    :return: url to try download sentence.
     """
     year = sentence.split("-")[-1]
     year = re.sub(r"[^a-zA-Z0-9]", "", year)
@@ -37,11 +39,11 @@ def get_url_sentence(sentence):
         return url_sentence
 
 
-def get_json_sentences_urls(folder):
+def get_json_sentences_urls(folder: pathlib.Path) -> None:
     """
-    Obtenemos archivo JSON con la información de sentencia (key) y su url (value).
-    :param folder: Folder con los archivos excel (.xlsx) donde se detallan las sentencias.
-    :return:
+    A JSON file is obtained with the sentence (id_sentence) information (key) and its url (value).
+    :param folder: Folder with the Excel files (.xlsx) where the sentences are detailed. folder must be a Path object.
+    :return: None
     """
     if not (MAIN_PATH / "data" / "sentences.json").exists():
         with open("data/sentences.json", "w") as file:
@@ -70,15 +72,24 @@ def get_json_sentences_urls(folder):
 
 
 def get_text_sentence_raw(sentence):
-    path_to_read = MAIN_PATH / "data" / "sentences_webscraping" / f"{sentence}.rtf"
-    with open(path_to_read, "r") as doc:
-        rtf_text = doc.read()
-    string_sentence = rtf_to_text(rtf_text)
-    return string_sentence
+    """
+    :param sentence: Sentence's name (Example: "C-010-95")
+    :return: String with the text raw in download sentence.
+    """
+    path_file_sentence = FOLDER_TO_READ / f"{sentence}.rtf"
+    try:
+        with open(path_file_sentence, "r") as doc:
+            rtf_text = doc.read()
+        string_sentence = rtf_to_text(rtf_text)
+        return string_sentence
+    except:
+        logging.exception(f"¡Have an exception in sentence: {sentence}!")
 
 
 class Sentences:
     """
+    The init is a mongodb's name of  in localhost. Must be existed the collections raw_texts and urls to use this class.
+
     Note: Start connexion with MongoDB in local --->  sudo systemctl start mongod
     """
 
@@ -86,50 +97,80 @@ class Sentences:
         self.db_name = db_name
         self.mongo_client = MongoClient(host="localhost", port=27017)
         self.database = None
-        self.db_collections = None
-        self.MAIN_PATH = Path(os.getcwd())
-        self.PATH_TO_READ = self.MAIN_PATH / "data" / "downloads_files_sentences"
-        self.PATH_TO_WRITE = self.MAIN_PATH / "data" / "preprocessing_corpus"
 
-    def _get_connection_database_mongo(self):
+    def _get_connection_mongodb(self) -> None:
         """
         Create a connection to the MongoDB DB from the init.
-        :return: A database object
         """
         if self.db_name not in self.mongo_client.list_database_names():
-            raise ValueError(f"Database {self.db_name} does not exist. Must be create the DB in mongo first!")
+            raise ValueError(
+                f"Database {self.db_name} does not exist. Must be create the DB in mongo first!"
+            )
         self.database = self.mongo_client[self.db_name]
-        return self.database
 
-    def get_collection(self, name_collection):
+    def _get_collection(self, name_collection: str):
         """
         :param name_collection: Name of collection in MongoDB
-        :return: pymongo.collection.Collection
+        :return: pymongo.collection.Collection (name_collection)
         """
-        database = self._get_connection_database_mongo()
-        self.db_collections = self.database.list_collection_names()
-        if name_collection not in self.db_collections:
-            raise ValueError(f"The collection {name_collection} in MongoDB {self.db_name} does not exist.")
-        collection = database[name_collection]
-        return collection
+        self._get_connection_mongodb()
+        if name_collection not in self.database.list_collection_names():
+            raise ValueError(
+                f"The collection {name_collection} in MongoDB {self.db_name} does not exist."
+            )
+        return self.database[name_collection]
 
-    def write_sentence_in_mongodb(self, sentence, name_collection: str):
+    def _write_in_collection_raw_texts(self, sentence):
         """
-        This function write in mongoDB the sentence_raw (Downloaded in .rft)
-        :param name_collection:
         :param sentence:
         :return:
         """
-        sentence_raw = get_text_sentence_raw(sentence)
-        document = {"id_sentence": sentence, "text_raw": sentence_raw}
-        # try:
-        existing_document = self.get_collection(name_collection).find_one({"id_sentence": document["id_sentence"]})
+        collection = self._get_collection("raw_texts")
+        text_sentence_raw = get_text_sentence_raw(sentence)
+        document = {"id_sentence": sentence, "text_raw": text_sentence_raw}
+        existing_document = collection.find_one(
+            {"id_sentence": document["id_sentence"]}
+        )
         if existing_document is not None:
-            raise ValueError(f"No es posible reescribir la información de la sentencia.!")
+            raise ValueError(
+                f"The sentence with id_sentence: {sentence} already exits in collection."
+            )
+        collection.insert_one(document)
 
-        self.get_collection(name_collection).insert_one(document)
-        self.mongo_client.close()
-        # except:
-        #     print(f"The database {self.db_name} has a new collection: {name_collection} OJO")
-        #     collection = self.database[name_collection]
-        #     collection.insert_one(document)
+    def _write_in_collection_urls(self, sentence):
+        """
+        :param sentence:
+        :return:
+        """
+        collection = self._get_collection("urls")
+        with open("data/sentences.json", "r") as f:
+            json_sentences = json.loads(f.read())
+        url_sentence = json_sentences[sentence]
+        document = {"id_sentence": sentence, "url": url_sentence}
+        existing_document = collection.find_one(
+            {"id_sentence": document["id_sentence"]}
+        )
+        if existing_document is not None:
+            raise ValueError(
+                f"The sentence with id_sentence: {sentence} already exits in collection."
+            )
+        collection.insert_one(document)
+
+    def write_info_sentence_in_collection(self, sentence: str, name_collection: str) -> None:
+        """
+        This function write info in mongodb's collection about sentence. id_sentence will be key identificator in all
+        collections.
+        :param sentence: Name of sentence to write.
+        :param name_collection: Collection where the information will be written.
+        :return:
+        """
+        if name_collection == "raw_texts":
+            self._write_in_collection_raw_texts(sentence)
+
+        elif name_collection == "urls":
+            self._write_in_collection_urls(sentence)
+# IDEA #
+# except:
+#     print(f"The database {self.db_name} has a new collection: {name_collection} OJO")
+#     collection = self.database[name_collection]
+#     collection.insert_one(document)
