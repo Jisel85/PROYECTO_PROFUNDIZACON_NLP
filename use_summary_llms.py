7e62d044bc3c8eeef25d633af0827a6d3484732b
Andres Campos <lacamposm@unal.edu.co>
2023-10-16 12:57:34 -0500

    * Three ways to generate summaries were built, 2 require API KEY
    * The folder with the test scripts was deleted

---
+++ use_summary_llms.py
@@ -0,0 +1,37 @@
+import logging
+from utils.judgments import MongoJudgments
+from utils.summarization import (
+    get_summarization_from_openai,
+    get_summarization_from_llm_gpt4all,
+    get_summarization_from_huggingface_api
+)
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logging.info(f"Start the process with LLM local")
+
+GPU_NAME = "NVIDIA GeForce RTX 3060 Laptop GPU"
+# ========================== Example with openai (Requirement OPENAI_KEY) ========================================= #
+judgment = MongoJudgments("T-006-10")  # (Long Judgment, 27 pages)
+raw_text = judgment.get_feature_of_judgment("raw_text")
+summary = get_summarization_from_openai(raw_text)
+print(summary)
+# ================================================================================================================= #
+
+# ========================================== Example with gpt4all ================================================= #
+judgment = MongoJudgments("T-273-01")  # (Short Judgment, 7 pages)
+raw_text = judgment.get_feature_of_judgment("raw_text")
+summary = get_summarization_from_llm_gpt4all(raw_text, model_name="llama-2-7b-chat", device=GPU_NAME)
+print(summary)
+# ================================================================================================================= #
+
+# =================================== Example with Huggingface API inference ======================================= #
+judgment = MongoJudgments("T-991-10")  # (Medium Judgment, 15 pages)
+raw_text = judgment.get_feature_of_judgment("raw_text")
+print(raw_text)
+summary = get_summarization_from_huggingface_api(raw_text)
+print(summary)
+# ================================================================================================================== #
+
+logging.info(f"Start the process with LLM local")
--- utils/summarization.py
+++ utils/summarization.py
@@ -1,87 +1,150 @@
-import torch
+import os
 import logging
 import tiktoken
-from utils.judgments import MongoJudgments
-from transformers import RobertaTokenizerFast, EncoderDecoderModel
-from langchain.chains.summarize import load_summarize_chain
+from pathlib import Path
+from transformers import LlamaTokenizerFast
+from langchain.globals import set_llm_cache
+from langchain.cache import InMemoryCache
+from langchain.llms import GPT4All, HuggingFaceHub
 from langchain.prompts import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.summarize import load_summarize_chain
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
+set_llm_cache(InMemoryCache())
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
+HUGGINGFACEHUB_API_TOKEN = os.environ["HUGGINGFACEHUB_API_TOKEN"]
+MAIN_PATH_MODELS_GPT4ALL = Path("/home/andres-campos/.cache/gpt4all/")
 
-def generate_summary_roberta(text):
-    """
-    :param text:
-    :return:
-    """
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    model = "Narrativa/bsc_roberta2roberta_shared-spanish-finetuned-mlsum-summarization"
-    tokenizer = RobertaTokenizerFast.from_pretrained(model)
-    model = EncoderDecoderModel.from_pretrained(model).to(device)
-    inputs = tokenizer(
-        [text],
-        padding="max_length",
-        truncation=True,
-        max_length=512,
-        return_tensors="pt",
+
+def count_tokens_from_openai(string: str) -> int:
+    """Returns the number of tokens in a text string with openai embeddings."""
+    encoding = tiktoken.get_encoding("cl100k_base")
+    return len(encoding.encode(string))
+
+
+def count_tokens_from_gpt4all(text: str) -> int:
+    """Returns the number of tokens in a text string with LlamA embeddings."""
+    tokenizer = LlamaTokenizerFast.from_pretrained(
+        pretrained_model_name_or_path="meta-llama/Llama-2-7b-chat-hf",
+        token=HUGGINGFACEHUB_API_TOKEN
     )
-    input_ids = inputs.input_ids.to(device)
-    attention_mask = inputs.attention_mask.to(device)
-    output = model.generate(input_ids, attention_mask=attention_mask)
-    return tokenizer.decode(output[0], skip_special_tokens=True)
+    return tokenizer.encode(text)
 
 
-def num_tokens_from_string(string: str, encoding_name="gpt-3.5-turbo") -> int:
-    """Returns the number of tokens in a text string."""
-    encoding = tiktoken.encoding_for_model(encoding_name)
-    num_tokens = len(encoding.encode(string))
-    return num_tokens
-
-
-def get_docs_judgment_to_openai(judgment):
+def get_documents_from_judgment(string_text, model="openai"):
     """
-    :param str judgment: id_judgment
+    Gets the judgment documents of the context size according to the structure that will be used for the summary
     :return:
     """
-    text_raw_judgment = MongoJudgments(judgment).get_feature_of_judgment(feature="raw_text")
+    mapper_chunck_size = {
+        "openai": 10000,
+        "gpt4all": 6500,
+        "huggingface_local": 10000,
+        "huggingface_api": 5000
+
+    }
     text_splitter = RecursiveCharacterTextSplitter(
         separators=["\n\n", "\n"],
-        chunk_size=10000,
-        chunk_overlap=500,
+        chunk_size=mapper_chunck_size[model],
+        chunk_overlap=mapper_chunck_size[model] * 0.05,  # Dejemos el 5% de overlapping.
         add_start_index=True,
     )
-    docs = text_splitter.create_documents([text_raw_judgment])
+    docs = text_splitter.create_documents([string_text])
     return docs
 
 
-def get_summarization_from_openai(judgment):
-    """
-    :param judgment:
-    :return:
-    """
-    llm = ChatOpenAI(temperature=0)
-    docs = get_docs_judgment_to_openai(judgment)
-
-    map_prompt = """La respuesta debe estar en español.
-    Escribe un resumen conciso de lo siguiente:
+def create_summary_chain(llm):
+    """Create a summary chain to apply map reduce"""
+    # Generals templates of summarize judgment #
+    map_prompt = """Your final answer must be in Spanish.
+    Write a concise summary of the following:
     {text}
     RESUMEN CONCISO:"""
 
-    combine_prompt = """La respuesta debe estar en español.
-    Escribe un resumen conciso de lo siguiente:
+    combine_prompt = """The answer must be in Spanish.
+    Write a concise summary of the following:
     {text}
-    Retorna tu respuesta con numerales los cuales cubren los puntos claves del texto.    
-    RESUMEN CON NUMERALES:"""
+    Return your answer which cover the key points of the text.   
+    SUMMARY IN SPANISH WITH NUMERALS:"""
+    # Generals templates of summarize judgment #
 
     summary_chain = load_summarize_chain(
         llm=llm,
         chain_type="map_reduce",
         map_prompt=PromptTemplate.from_template(map_prompt),
         combine_prompt=PromptTemplate.from_template(combine_prompt),
-        # verbose=True
+        verbose=True
     )
+    return summary_chain
+
+
+def get_summarization_from_openai(text):
+    """
+    :param text:
+    :return:
+    """
+    llm = ChatOpenAI(temperature=0)
+    docs = get_documents_from_judgment(text, model="openai")
+    summary_chain = create_summary_chain(llm)
+
+    return summary_chain.run(docs)
+
+
+def get_summarization_from_llm_gpt4all(text, model_name="orca-mini-3b", device="cpu"):
+    """
+    This function obtain summary of judgment using GPT4ALL models (https://gpt4all.io/index.html).
+    Model support: llama-2-7b-chat, orca-mini-13b, orca-mini-7b, orca-mini-3b, gpt4all-falcon, GPT4All-13B-snoozy,
+    wizardlm-13b-v1.1-superhot-8k (Recommended)
+
+    :param str text: Text like a string to summarize.
+    :param model_name:
+    :param device: Optional. Name your GPU, for example: NVIDIA GeForce RTX 3060 Laptop GPU
+    :return:
+    """
+    if model_name == "gpt4all-falcon":
+        path_model = MAIN_PATH_MODELS_GPT4ALL / "ggml-model-gpt4all-falcon-q4_0.bin"
+    else:
+        path_model = MAIN_PATH_MODELS_GPT4ALL / f"{model_name}.ggmlv3.q4_0.bin"
+
+    callbacks = [StreamingStdOutCallbackHandler()]
+    llm = GPT4All(
+        model=str(path_model),
+        callbacks=callbacks,
+        max_tokens=1024,
+        verbose=True,  # Verbose is required to pass to the callback manager
+        n_threads=os.cpu_count(),
+        device=device
+    )
+    docs = get_documents_from_judgment(text, model="gpt4all")
+    summary_chain = create_summary_chain(llm)
+
+    return summary_chain.run(docs)
+
+
+def get_summarization_from_huggingface_api(text, model_name="mistralai/Mistral-7B-Instruct-v0.1"):
+    """
+    Obtenemos resumenes de documentos largos haciendo uso de la API inference de Huggingface (Necesita API KEY)
+    :param str text: Large text to be summarize.
+    :param str model_name: Model name to use from huggingface host.
+    :return: Summary of the tetx.
+    """
+    repo_id = model_name
+    llm = HuggingFaceHub(
+        task="text-generation",
+        repo_id=repo_id,
+        verbose=True,
+        model_kwargs={
+            "temperature": 0.001,
+            "max_new_tokens": 600,
+        }
+    )
+    docs = get_documents_from_judgment(text, model="huggingface_api")
+    summary_chain = create_summary_chain(llm)
+
     return summary_chain.run(docs)
